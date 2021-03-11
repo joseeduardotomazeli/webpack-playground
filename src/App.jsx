6c3a939341e89b27d9dd7a3c11f289582169552e
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-03-11 13:34:17 -0300

    chore: add webpack config

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import './styles.css'
+
+function App() {
+  const [counter, setCounter] = useState(0)
+
+  function increaseCounter() {
+    setCounter((counter) => counter + 1)
+  }
+
+  return (
+    <div className="container">
+      <h1>Webpack Playground</h1>
+
+      <button type="button" onClick={increaseCounter}>
+        Counter: {counter}
+      </button>
+    </div>
+  )
+}
+
+export default App
