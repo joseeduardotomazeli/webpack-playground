8863873599d5648cdf8f9357c168c1e8d222672b
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2022-06-25 01:29:29 -0300

    chore: add styles loader

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,11 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
 };
